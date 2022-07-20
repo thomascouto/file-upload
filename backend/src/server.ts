121b3b774989b2902526571045c64d5d68fd7996
Thomas Couto (DESKTOP) <thomascouto@gmail.com>
2022-07-19 21:44:27 -0300

    refactor: entry files

---
+++ backend/src/server.ts
@@ -0,0 +1,38 @@
+import express, { Express } from 'express'
+import { setupRoutes } from './config/setupRoutes'
+import logger from 'morgan'
+import { appCors } from './middlewares/cors'
+export class Server {
+	private app: Express
+	private port: string | number = process.env.SERVER_PORT ?? 3000
+
+	private setupMiddlewares(): Express {
+		if (process.env.TS_NODE_DEV) {
+			console.log('TS_NODE_DEV=TRUE')
+			this.app.use(logger('dev'))
+			this.app.use(appCors)
+		}
+		return this.app
+	}
+
+	private setupRoutes(): Express {
+		setupRoutes(this.app)
+		return this.app
+	}
+
+	private listen() {
+		this.app.listen(this.port, () => {
+			console.info(`Server listening on port ${this.port}`)
+		})
+	}
+
+	public start() {
+		this.setupMiddlewares()
+		this.setupRoutes()
+		this.listen()
+	}
+
+	constructor() {
+		this.app = express().set('trust proxy', 1).disable('x-powered-by')
+	}
+}
